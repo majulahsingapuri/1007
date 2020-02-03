c9acbcca7d73fec6c4f1ef54586691e184d99787
majulahsingapuri <bhsingapuri@gmail.com>
2020-02-03 23:21:33 +0800

    transpose2D

---
+++ week 4/transpose2D/transpose2D.c
@@ -0,0 +1,61 @@
+//
+//  main.c
+//  transpose2D
+//
+//  Created by Bhargav Singapuri on 3/2/20.
+//  Copyright © 2020 Bhargav Singapuri. All rights reserved.
+//
+
+#include <stdio.h>
+#define SIZE 10
+
+void transpose2D(int ar[][SIZE], int rowSize, int colSize);
+void display(int ar[][SIZE], int rowSize, int colSize);
+
+int main() {
+      
+    int ar[SIZE][SIZE], rowSize, colSize;
+    int i,j;
+    
+    printf("Enter row size of the 2D array: \n");
+    scanf("%d", &rowSize);
+    printf("Enter column size of the 2D array: \n");
+    scanf("%d", &colSize);
+    printf("Enter the matrix (%dx%d): \n", rowSize, colSize);
+    
+    for (i=0; i<rowSize; i++) {
+        for (j=0; j<colSize; j++) {
+            scanf("%d", &ar[i][j]);
+        }
+    }
+    
+    printf("transpose2D(): \n");
+    transpose2D(ar, rowSize, colSize);
+    display(ar, rowSize,    colSize);
+    
+    return 0;
+}
+   
+void display(int ar[][SIZE], int rowSize, int colSize) {
+    int l,m;
+    for (l = 0; l < rowSize; l++) {
+        for (m = 0; m < colSize; m++) {
+            printf("%d ", ar[l][m]);
+        }
+    printf("\n");
+    }
+
+}
+   
+void transpose2D(int ar[][SIZE], int rowSize, int colSize) {
+    
+    int temp;
+    
+    for (int i=0; i<rowSize; i++) {
+        for (int j=0; j< i; j++) {
+            temp = ar[i][j];
+            ar[i][j] = ar[j][i];
+            ar[j][i] = temp;
+        }
+    }
+}
