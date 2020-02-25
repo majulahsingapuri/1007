8ce46d74e20757d742ed986c0c306af35c86abe6
Bhargav Singapuri <bhsingapuri@gmail.com>
2020-02-25 17:36:09 +0800

    rFindMaxAr

---
+++ Week 7/rFindMaxAr/rFindMaxAr.c
@@ -0,0 +1,40 @@
+//
+//  main.c
+//  rFindMaxAr
+//
+//  Created by Bhargav Singapuri on 25/2/20.
+//  Copyright © 2020 Bhargav Singapuri. All rights reserved.
+//
+
+#include <stdio.h>
+      
+void rFindMaxAr(int *a, int size, int *max);
+      
+int main() {
+         
+    int ar[50],i,max,size;
+    printf("Enter array size: \n");
+    scanf("%d", &size);
+    printf("Enter %d numbers: \n", size);
+    
+    for (i=0; i < size; i++)
+        scanf("%d", &ar[i]); max=ar[0];
+    rFindMaxAr(ar,size,&max);
+    printf("rFindMaxAr(): %d\n", max);
+    
+    return 0;
+}
+     
+void rFindMaxAr(int *ar, int size, int *max) {
+    
+    if (size == -1) {
+        *max = ar[size + 1];
+        return;
+    }
+    
+    rFindMaxAr(ar, size - 1, max);
+    
+    if (ar[size - 1] > *max) {
+        *max = ar[size];
+    }
+}
