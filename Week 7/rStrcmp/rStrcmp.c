fd18dbc39f04082f4371cecfb49514dd6a3b06b1
NicholasGoh <gohn0004@e.ntu.edu.sg>
2020-03-03 09:46:01 +0800

    Update rStrcmp.c

--- Week 7/rStrcmp/rStrcmp.c
+++ Week 7/rStrcmp/rStrcmp.c
@@ -54,6 +54,7 @@ int rStrcmp(char *s1, char *s2)
     if (*s1 > *s2) return 1;
     if (*s1 < *s2) return -1;
     else{
+        if (*s1 == '\0' && *s2 == '\0') return 0;
         *s1++; *s2++;
         if (*s1 != '\0' || *s2 != '\0')
             return 0 + rStrcmp(s1, s2);
